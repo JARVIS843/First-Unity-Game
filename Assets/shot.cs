d7f761f75642dc3bb99f8fa83edba8de80cea30b
Jarvis Yang <46472696+niceTimnice@users.noreply.github.com>
2020-08-06 16:08:24 +0800

    Fist commit

---
+++ Assets/shot.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shot : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    Ray ray;
+    RaycastHit hit;
+    public LayerMask layer;
+
+    // Update is called once per frame
+    void Update()
+    {
+        ray = Camera.main.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0));
+        if(Physics.Raycast(ray,out hit, 50, layer))
+        {
+            if (Input.GetMouseButtonDown(0)){
+                Destroy(hit.transform.gameObject);
+            }
+        }
+    }
+}
